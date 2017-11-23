ed9094475a50fc5386a66ce1e078a873e85cfdf2
Marcio Montenegro <31051083+marciowelben@users.noreply.github.com>
2017-11-23 00:49:32 -0300

    Route rename

--- api/app/v1/route/user.php
+++ api/app/v1/route/user.php
@@ -5,8 +5,8 @@ $app->group('/v1', function () {
     $this->group('/user', function () {
 
         $this->POST('', App\v1\Api\UserApi::class . ':createUser');
-        $this->GET('', App\v1\Api\UserApi::class . ':login');
-        $this->GET('/list', App\v1\Api\UserApi::class . ':userAsList');
+        $this->GET('', App\v1\Api\UserApi::class . ':loginUser');
+        $this->GET('/list', App\v1\Api\UserApi::class . ':getUsersAsList');
         $this->GET('/{id}', App\v1\Api\UserApi::class . ':getUserById');
         $this->DELETE('/{id}', App\v1\Api\UserApi::class. ':deleteUser');
         $this->PUT('/{id}', App\v1\Api\UserApi::class. ':editUser');
